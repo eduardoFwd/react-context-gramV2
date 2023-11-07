9c9a342edee3b8fb30f8178acbe61a02bf7505e8
Pablo Arce <josepa_arce@hotmail.com>
2023-11-06 18:04:18 -0600

    FeedContext Info

---
+++ src/context/FeedContext.js
@@ -0,0 +1,65 @@
+// Create a context that holds the time line feed of a user.
+// You can use the next data; feel free to modify the values.
+// IMPORTANT! Keep the existing attributes; feel free to add more if it is convenient.
+/*
+
+    const followedAccounts = [
+      {
+        uid: 1,
+        username: 'Elza Nahoria Gourmet'
+        thumbnail: 'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?q=80&w=2934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      },
+      {
+        uid: 2,
+        username: 'Juan Frustrante'
+        thumbnail: 'https://plus.unsplash.com/premium_photo-1689531953275-a5cfcc458931?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      },
+      {
+        uid: 3,
+        username: 'Rita'
+        thumbnail: 'https://plus.unsplash.com/premium_photo-1690579805273-fd0c7b08035d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      }
+    ];
+  
+    const timelineFeed = [
+      {
+        uid: 1,
+        user: 1,
+        likes: 999,
+        description: 'My top 3 pizzas! from el puerto!'
+        location: 'Puntarenas, Costa Rica',
+        pictures: [
+          {
+            url: 'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg',
+            url: 'https://cdn.pixabay.com/photo/2017/02/15/10/57/pizza-2068272_1280.jpg',
+            url: 'https://cdn.pixabay.com/photo/2017/09/30/15/10/plate-2802332_1280.jpg'
+          }
+        ]
+      },
+      {
+        uid: 2,
+        user: 2,
+        likes: 44,
+        description: 'About Last Night!'
+        location: 'Las Vegas, Nevada',
+        pictures: [
+          {
+            url: 'https://cdn.pixabay.com/photo/2016/09/10/11/11/musician-1658887_1280.jpg',
+          }
+        ]
+      },
+      {
+        uid: 3,
+        user: 3,
+        likes: 6000,
+        description: 'About Last Night!'
+        location: 'Las Vegas, Nevada',
+        pictures: [
+          {
+            url: 'https://cdn.pixabay.com/photo/2017/12/20/10/48/portugal-3029665_1280.jpg',
+            url: 'https://cdn.pixabay.com/photo/2017/01/08/19/30/rio-de-janeiro-1963744_1280.jpg',
+          }
+        ]
+      }
+    ]
+ */
